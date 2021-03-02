725fa421fb20f745030afb20d388535c128c99c5
kakikubo <t.kakikubo@fitsplus.co.jp>
2021-03-03 06:47:44 +0900

    scopeの定義を追加してみる

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+  default_scope -> { order("published_on desc") }
+  scope :costly, -> { where("price > ?", 3000)}
+  scope :written_about, ->(theme) { where("name like ?", "%#{theme}%") }
+  scope :find_price, ->(price) { find_by(price: price) } # NOTE: find_byがnilを返してしまう為、条件を指定していないのと同じになってしまう
 end

15e6e5a32fea32175c0121b45e78321dcba545c1
kakikubo <t.kakikubo@fitsplus.co.jp>
2021-03-04 09:08:11 +0900

    コントローラの処理とフックを実装。aroundの使い方に注意(yieldがある)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,31 @@
 class BooksController < ApplicationController
+  protect_from_forgery except: [:destroy] # テスト用にCSRF対策をOFFにする
+  before_action :set_book, only: [:show, :destroy]
+  around_action :action_logger
   def show
-    @book = Book.find(params[:id])
     respond_to do |format|
       format.html
       format.json
     end
+  end
+
+  def destroy
+    @book.destroy
+    respond_to do |format|
+      format.html { redirect_to "/" }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def action_logger
+    Rails.logger.info "( ﾟдﾟ)ﾊｯ!"
+    yield
+    Rails.logger.info "(　>д<)､;'.･　ｨｸｼｯ'"
   end
 end

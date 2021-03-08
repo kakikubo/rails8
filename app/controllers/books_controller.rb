36ea5c8f06dbd40421202ad38336ece44ed2d038
kakikubo <t.kakikubo@fitsplus.co.jp>
2021-03-09 06:54:52 +0900

    こういう書き方もできるよという例

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,10 @@
 class BooksController < ApplicationController
   protect_from_forgery except: [:destroy] # テスト用にCSRF対策をOFFにする
   before_action :set_book, only: [:show, :destroy]
+  # こういう書き方もできるよという例
+  # before_action do
+  #   redirect_to access_denied_path if params[:token].blank?
+  # end
   around_action :action_logger
   def show
     respond_to do |format|

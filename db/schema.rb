ff7f867892fb766df25dcab37244ed1dd4a37b6a
kakikubo <t.kakikubo@fitsplus.co.jp>
2021-03-10 07:33:16 +0900

    dip rails g scaffold comment name:string body:text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_232128) do
+ActiveRecord::Schema.define(version: 2021_03_09_220727) do
+
+  create_table "comments", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"

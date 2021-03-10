1f41accfcfd6764d756dd21c5eae3cc3447cf8fe
kakikubo <t.kakikubo@fitsplus.co.jp>
2021-03-11 08:41:14 +0900

    AsyncLogJobのminitestを追加。retry_onの書式サンプル追加

--- app/jobs/async_log_job.rb
+++ app/jobs/async_log_job.rb
@@ -1,5 +1,4 @@
 class AsyncLogJob < ApplicationJob
-  queue_as :async_log
   queue_as do
     case self.arguments.first[:message]
     when "to async_log"
@@ -7,6 +6,13 @@ class AsyncLogJob < ApplicationJob
     else
       :default
     end
+  end
+
+  retry_on StandardError, wait: 5.seconds, attempts: 3
+  # 複数の例外クラスを指定する事も可能
+  # retry_on ArgumentError, ZeroDivisionError, wait: 5.seconds, attempts: 3
+  discard_on StandardError do |job, error|
+    # SomeNotifier.push(error)
   end
 
   def perform(message: "Hello")

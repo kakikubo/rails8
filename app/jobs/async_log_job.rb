73b4439e39ad632f15f7d584a1f20234f046d939
kakikubo <t.kakikubo@fitsplus.co.jp>
2021-03-11 08:27:57 +0900

    AsyncLogJobの引数に応じてキューを切り替える例

--- app/jobs/async_log_job.rb
+++ app/jobs/async_log_job.rb
@@ -1,5 +1,13 @@
 class AsyncLogJob < ApplicationJob
   queue_as :async_log
+  queue_as do
+    case self.arguments.first[:message]
+    when "to async_log"
+      :async_log
+    else
+      :default
+    end
+  end
 
   def perform(message: "Hello")
     # Do something later

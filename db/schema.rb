613b78e489bc6e942b7f54c80cc38cce046e6144
kakikubo <kakikubo@gmail.com>
2021-03-16 07:52:08 +0900

    ActionMailboxに関してのサンプルコードだが、ここはSendgridの設定なども必要だったので、実戦で学ぶ事にする FIXME

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_215004) do
+ActiveRecord::Schema.define(version: 2021_03_15_222513) do
+
+  create_table "action_mailbox_inbound_emails", charset: "utf8mb4", force: :cascade do |t|
+    t.integer "status", default: 0, null: false
+    t.string "message_id", null: false
+    t.string "message_checksum", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["message_id", "message_checksum"], name: "index_action_mailbox_inbound_emails_uniqueness", unique: true
+  end
 
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -52,11 +61,19 @@ ActiveRecord::Schema.define(version: 2021_03_11_215004) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "comments", charset: "utf8mb4", force: :cascade do |t|
-    t.string "name"
-    t.text "body"
+  create_table "boards", charset: "utf8mb4", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", charset: "utf8mb4", force: :cascade do |t|
+    t.string "body", null: false
+    t.bigint "creator_id", null: false
+    t.bigint "board_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["board_id"], name: "index_comments_on_board_id"
+    t.index ["creator_id"], name: "index_comments_on_creator_id"
   end
 
   create_table "entries", charset: "utf8mb4", force: :cascade do |t|
@@ -77,5 +94,7 @@ ActiveRecord::Schema.define(version: 2021_03_11_215004) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "boards"
+  add_foreign_key "comments", "users", column: "creator_id"
   add_foreign_key "entries", "blogs"
 end

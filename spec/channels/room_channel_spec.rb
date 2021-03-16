f7d036323522e9fb780a98ad58039e1f9232c06b
kakikubo <t.kakikubo@fitsplus.co.jp>
2021-03-17 08:54:20 +0900

    ちょっとだけActionCableのテストを書くなど。

--- spec/channels/room_channel_spec.rb
+++ spec/channels/room_channel_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe RoomChannel, type: :channel do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "subscribes" do
+    it {
+      subscribe
+      expect(subscription.confirmed?).to be_truthy
+      expect(subscription).to have_stream_from("room_channel")
+    }
+    it {
+      subscribe
+      expect(subscription.confirmed?).to be_truthy
+      expect(subscription).not_to have_stream_from("bad_channel")
+    }
+  end
+
+  describe "broadcast" do
+    let(:text){ "Hello"}
+    let(:broadcast_text){ ApplicationController.render(
+      partial: 'message2s/message2',
+      locals: { message2: Message2.new(content: text)}
+    )}
+    xit {
+      # ここはminitestだと以下のようなテストになる筈
+      # assert_broadcast_on('room_channel', message: broadcast_text) do
+      #   perform :speak, message: text
+      # end
+      subscribe
+      expect {
+        ActionCable.server.broadcast(
+          "room_channel", text: broadcast_text
+        )
+      }.to have_broadcasted_to("room_channel").with("Hello")
+    }
+  end
 end

00f12727207ebf2efc39e9a65f730706a8b549e8
kakikubo <t.kakikubo@fitsplus.co.jp>
2021-03-24 08:50:24 +0900

    EventControllerのテスト。request specで実装

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Events", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "自分でつくったイベントは" do
+    context "削除がせいこうすること" do
+      let(:event_owner){ FactoryBot.create(:user) }
+      let(:event){ FactoryBot.create(:event, owner: event_owner) }
+      before do
+        sign_in_as event_owner
+        delete event_path(id: event.id)
+      end
+
+      it { expect(Event.all.size).to eq 0 }
+    end
+
+
+
   end
 end

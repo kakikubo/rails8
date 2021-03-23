e80869e09cc93761a0ceabb93a00d4a93a969427
kakikubo <t.kakikubo@fitsplus.co.jp>
2021-03-24 07:14:39 +0900

    /status へのアクセスをrequest specで実装

--- spec/requests/status_spec.rb
+++ spec/requests/status_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Statuses", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+
+  describe "GET /status" do
+    before { get status_path }
+
+    context "response" do
+      it "成功のステータスが返ること" do
+        expect(response.status).to eq 200
+      end
+      it "レスポンスにjsonでokが入っていること" do
+        json = JSON.parse(response.body)
+        expect(json['status']).to eq "ok"
+      end
+      it "content-typeがapplication/jsonになっていること" do
+        expect(response.media_type).to eq "application/json"
+      end
+    end
   end
 end

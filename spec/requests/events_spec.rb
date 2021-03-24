c912740c33ce140e7e0518552e37a5289a421082
kakikubo <t.kakikubo@fitsplus.co.jp>
2021-03-24 09:02:19 +0900

    イベントの削除がきちんとできる事

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Events", type: :request do
   describe "自分でつくったイベントは" do
-    context "削除がせいこうすること" do
-      let(:event_owner){ FactoryBot.create(:user) }
-      let(:event){ FactoryBot.create(:event, owner: event_owner) }
+    context "削除が成功すること" do
+      let(:event_owner) { FactoryBot.create(:user) }
+      let(:event) { FactoryBot.create(:event, owner: event_owner) }
       before do
         sign_in_as event_owner
         delete event_path(id: event.id)
@@ -12,8 +12,22 @@ RSpec.describe "Events", type: :request do
 
       it { expect(Event.all.size).to eq 0 }
     end
+    describe "他人がつくったイベントは" do
+      context "削除が失敗すること" do
+        let(:event_owner) { FactoryBot.create(:user) }
+        let(:user) { FactoryBot.create(:user) }
+        let(:event) { FactoryBot.create(:event, owner: event_owner) }
+        before do
+          sign_in_as user
+        end
 
-
-
+        it {
+          expect do
+            delete event_path(id: event.id)
+            Event.all.size
+          end.to raise_error(ActiveRecord::RecordNotFound)
+        }
+      end
+    end
   end
 end

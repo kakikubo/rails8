83beba911fdf7e65f2d6cd3fd292eea153000691
kakikubo <t.kakikubo@fitsplus.co.jp>
2021-03-26 08:11:06 +0900

    active_storage_validationを導入。デフォルトのimagemagickではなくvipsを利用するように変更。

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,6 @@ module App
     # config.eager_load_paths << Rails.root.join("extras")
     # data-remote: trueをデフォルトでつける。本来ついている筈だが有効になってなかったので明示的に指定
     config.action_view.form_with_generates_remote_forms = true
+    config.active_storage.variant_processor = :vips
   end
 end

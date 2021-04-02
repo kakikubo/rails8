001f2cfbf92ceb0000a94e2babc87ce672dec256
kakikubo <kakikubo@gmail.com>
2021-04-03 08:42:33 +0900

    超絶簡単なテストを行う

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,4 +4,13 @@ require 'rails_helper'
 
 RSpec.describe Event, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "正常なパラメータが入力された場合" do
+    context 'イベントは' do
+      let(:event){ FactoryBot.create(:event) }
+      it "正しくイベントが作成されること" do
+        expect(event).to be_valid
+      end
+    end
+  end
 end

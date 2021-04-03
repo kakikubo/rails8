42582f1d9b5fb0a108cbdb735095ea863a7ef406
kakikubo <kakikubo@gmail.com>
2021-04-03 09:22:22 +0900

    簡単なModelスペックを記載

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,13 +3,57 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-
-  describe '正常なパラメータが入力された場合' do
-    context 'イベントは' do
-      let(:event) { create(:event) }
+  describe '正常系' do
+    context '正常なパラメータが入力されたイベントは' do
+      let(:event) { build(:event) }
       it '正しくイベントが作成されること' do
         expect(event).to be_valid
+      end
+    end
+    context '画像が指定されていない場合' do
+      let(:event) { build(:event, image: nil) }
+      it 'イベントが作成されること' do
+        expect(event).to be_valid
+        expect(event.errors[:image]).not_to eq(['を入力してください'])
+      end
+    end
+  end
+
+  describe '異常系' do
+    context '主催者が指定されていない場合' do
+      let(:event) { build(:event, owner: nil) }
+      it 'イベントが作成されないこと' do
+        expect(event).not_to be_valid
+        expect(event.errors[:owner]).to eq(['を入力してください'])
+      end
+    end
+
+    context '名前が指定されていない場合' do
+      let(:event) { build(:event, name: nil) }
+      it 'イベントが作成されないこと' do
+        expect(event).not_to be_valid
+        expect(event.errors[:name]).to eq(['を入力してください'])
+      end
+    end
+    context '場所が指定されていない場合' do
+      let(:event) { build(:event, place: nil) }
+      it 'イベントが作成されないこと' do
+        expect(event).not_to be_valid
+        expect(event.errors[:place]).to eq(['を入力してください'])
+      end
+    end
+    context '開始時間が指定されていない場合' do
+      let(:event) { build(:event, :no_start_at) }
+      it 'イベントが作成されないこと' do
+        expect(event).not_to be_valid
+        expect(event.errors[:start_at]).to eq(['を入力してください'])
+      end
+    end
+    context '終了時間が指定されていない場合' do
+      let(:event) { build(:event, :no_end_at) }
+      it 'イベントが作成されないこと' do
+        expect(event).not_to be_valid
+        expect(event.errors[:end_at]).to eq(['を入力してください'])
       end
     end
   end

b5462705f6f6c1967205e997a6fe5e84ee3c1295
kakikubo <kakikubo@gmail.com>
2021-04-15 08:18:07 +0900

    ticket modelのバリデーションだけテスト

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -3,5 +3,37 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    let(:user) { create(:user) }
+    let(:event) { create(:event) }
+    context '正常系' do
+      it 'バリデーションを通過すること' do
+        ticket = described_class.new(
+          user: user,
+          event: event,
+          comment: SecureRandom.alphanumeric((0..30).to_a.sample)
+        )
+        expect(ticket).to be_valid
+      end
+      it 'ユーザが紐付いていなくてもバリデーションは通過すること' do
+        ticket = described_class.new(
+          user: nil,
+          event: event,
+          comment: SecureRandom.alphanumeric((0..30).to_a.sample)
+        )
+        expect(ticket).to be_valid
+      end
+    end
+    context '異常系' do
+      it 'バリデーションを通過しないこと' do
+        ticket = described_class.new(
+          user: user,
+          event: event,
+          comment: SecureRandom.alphanumeric(31)
+        )
+        expect(ticket).not_to be_valid
+      end
+    end
+
+  end
 end

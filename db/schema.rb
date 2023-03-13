ffa99d75744bf3cf72bd55a098ede331f29c38ab
mpg-teruo-kakikubo <teruo.kakikubo@medpeer.co.jp>
2023-03-13 09:37:45 +0900

    rails app:update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2021_03_24_220117) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_003714) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -28,7 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2021_03_24_220117) do
     t.text "metadata"
     t.string "service_name", null: false
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
+    t.string "checksum"
     t.datetime "created_at", precision: nil, null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
